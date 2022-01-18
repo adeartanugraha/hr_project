0e5e2133ec5ff5cf040ddc4d5c141ef0d215218c
adearta <bagusmadearta01@gmail.com>
2022-01-18 11:00:46 +0800

    :sparkles: make new tabel in database sql

--- database/db_hrproject.sql
+++ database/db_hrproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 18, 2022 at 03:06 AM
+-- Generation Time: Jan 18, 2022 at 03:59 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.4.21
 
@@ -24,6 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `data_absent`
+--
+
+CREATE TABLE `data_absent` (
+  `id_absent` bigint(20) NOT NULL,
+  `id_user` bigint(20) NOT NULL,
+  `id_industry` bigint(20) NOT NULL,
+  `check_in_time` datetime NOT NULL,
+  `check_out_time` datetime NOT NULL,
+  `is_late` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_industry`
+--
+
+CREATE TABLE `data_industry` (
+  `id_industry` bigint(20) NOT NULL,
+  `name_industry` varchar(200) NOT NULL,
+  `status` tinyint(4) NOT NULL COMMENT '0 for WFO and 1 for WFH'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `data_user`
 --
 
@@ -39,6 +66,20 @@ CREATE TABLE `data_user` (
 
 INSERT INTO `data_user` (`id_user`, `username`, `password`) VALUES
 (846067400050653, 'bagusmade', 'adesaga07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_workhour`
+--
+
+CREATE TABLE `data_workhour` (
+  `id_workhour` bigint(20) NOT NULL,
+  `id_industry` bigint(20) NOT NULL,
+  `day` tinyint(4) NOT NULL,
+  `time_in` time NOT NULL,
+  `time_out` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
