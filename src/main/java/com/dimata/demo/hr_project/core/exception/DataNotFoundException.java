ed4aad138a750f96f6f26243b79986c3bf871a41
adearta <bagusmadearta01@gmail.com>
2022-02-23 10:52:55 +0800

    :tada: make return error 404 status

--- src/main/java/com/dimata/demo/hr_project/core/exception/DataNotFoundException.java
+++ src/main/java/com/dimata/demo/hr_project/core/exception/DataNotFoundException.java
@@ -1,16 +1,21 @@
 package com.dimata.demo.hr_project.core.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Gunakan exception ini jika data tidak tersedia di database.
  *
  * @author Hariyogi
  * @since 2 Sep 2020
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class DataNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 5558238161985653783L;
     private static final String DEFAULT_POSTFIX = " tidak ditemukan";
     private final String message;
 
+    
     public DataNotFoundException(String parameter) {
         super(parameter);
         this.message = parameter;
